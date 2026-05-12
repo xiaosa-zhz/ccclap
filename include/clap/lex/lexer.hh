f2b3d79b8ce5bdc04f90ec7f052bebb9d8bf928b
xiaosa@work <dr.xiaosa@gmail.com>
2026-05-12 17:38:04 +0800

    Use charconv

--- include/clap/lex/lexer.hh
+++ include/clap/lex/lexer.hh
@@ -2,9 +2,8 @@
 #ifndef CCCLAP_LEX_LEXER_H
 #define CCCLAP_LEX_LEXER_H 1
 
-#include "clap/util/ascii.hh"
-
 #include <string_view>
+#include <charconv>
 
 namespace clap {
 
@@ -24,28 +23,12 @@ constexpr bool is_long_option(std::string_view arg) noexcept {
     return arg.size() >= 3 && arg.starts_with("--");
 }
 
-constexpr bool is_number(std::string_view s) noexcept {
-    if (s.empty()) return false;
-    bool has_point = false;
-    bool has_digit = false;
-    for (size_t i = 0; i < s.size(); ++i) {
-        char c = s[i];
-        if (ascii::is_digit(c)) {
-            has_digit = true;
-        } else if (c == '.') {
-            if (has_point) return false;
-            has_point = true;
-        } else {
-            return false;
-        }
-    }
-    return has_digit;
-}
-
 constexpr bool is_negative_number(std::string_view arg) noexcept {
     if (arg.size() < 2) return false;
     if (arg[0] != '-') return false;
-    return is_number(arg.substr(1));
+    std::size_t res;
+    auto [_, ec] = std::from_chars(arg.data() + 1, arg.data() + arg.size(), res);
+    return ec == std::errc();
 }
 
 } // namespace clap
