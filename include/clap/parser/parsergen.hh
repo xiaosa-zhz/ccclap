9b81110953c19b118315b98cd8910a3d8f9352eb
xiaosa@work <dr.xiaosa@gmail.com>
2026-05-19 17:41:15 +0800

    Add codegen header

---
+++ include/clap/parser/parsergen.hh
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef CCCLAP_PARSER_PARSER_GENERATOR_HH
+#define CCCLAP_PARSER_PARSER_GENERATOR_HH 1
+
+#include <meta>
+
+namespace clap {
+
+// TODO
+/*
+constexpr bool is_stdio() const noexcept {
+    return text == "-";
+}
+
+constexpr bool is_positional_escape() const noexcept {
+    return text == "--";
+}
+
+constexpr bool is_short_option() const noexcept {
+    return text.size() >= 2 && text[0] == '-' && text[1] != '-';
+}
+
+constexpr bool is_long_option() const noexcept {
+    return text.size() >= 3 && text.starts_with("--");
+}
+
+constexpr bool is_negative_number() const noexcept {
+    if (text.size() < 2) return false;
+    if (text[0] != '-') return false;
+    [[maybe_unused]] std::size_t res = 0;
+    auto [_, ec] = std::from_chars(text.data() + 1, text.data() + text.size(), res);
+    return ec == std::errc();
+}
+*/
+
+} // namespace clap
+
+#endif // CCCLAP_PARSER_PARSER_GENERATOR_HH
--- include/clap/parser/token.hh
+++ include/clap/parser/token.hh
@@ -4,54 +4,15 @@
 
 #include <concepts>
 #include <string_view>
-#include <charconv>
 #include <compare>
 #include <iterator>
 #include <ranges>
 
 namespace clap {
 
-enum class token_kind {
-    eof,
-    stdio,
-    positional_escape,
-    short_option,
-    long_option,
-    negative_number,
-    argument,
-    unknown,
-};
-
-// TODO:
-//  - try_parse_* and these function needs to adapt to result value type
-
 struct token {
     std::string_view text;
     std::string_view origin;
-
-    constexpr bool is_stdio() const noexcept {
-        return text == "-";
-    }
-
-    constexpr bool is_positional_escape() const noexcept {
-        return text == "--";
-    }
-
-    constexpr bool is_short_option() const noexcept {
-        return text.size() >= 2 && text[0] == '-' && text[1] != '-';
-    }
-
-    constexpr bool is_long_option() const noexcept {
-        return text.size() >= 3 && text.starts_with("--");
-    }
-
-    constexpr bool is_negative_number() const noexcept {
-        if (text.size() < 2) return false;
-        if (text[0] != '-') return false;
-        [[maybe_unused]] std::size_t res = 0;
-        auto [_, ec] = std::from_chars(text.data() + 1, text.data() + text.size(), res);
-        return ec == std::errc();
-    }
 };
 
 class token_view : public std::ranges::view_interface<token_view>
